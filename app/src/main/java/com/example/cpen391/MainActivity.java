726819facb18e55b8e9952c033eb02632914f76c
HizanLi <80601415+HizanLi@users.noreply.github.com>
2023-02-10 17:07:28 -0800

    Server connection need to update, somehow done

--- app/src/main/java/com/example/cpen391/MainActivity.java
+++ app/src/main/java/com/example/cpen391/MainActivity.java
@@ -16,42 +16,46 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         TextView usernameTV =(TextView) findViewById(R.id.username);
         TextView passwordTV =(TextView) findViewById(R.id.password);
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        //admin and admin
-
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // on below line we are getting data from our edit text.
                 String userName = usernameTV.getText().toString();
                 String password = passwordTV.getText().toString();
-                Log.d("home", getSha256Hash("admin"));
+//                Log.d("userName sha256-ed: ", getSha256Hash(userName));
+//                Log.d("password sha256-ed: ", getSha256Hash(password));
 
-                // checking if the entered text is empty or not.
-                if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(password)) {
-                    Toast.makeText(MainActivity.this, "Please enter user name and password", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
 
-                if(userName.equals("admin") && password.equals("admin")){
-                    //correct
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
-                    i.putExtra("username", userName);
-                    startActivity(i);
-                }else{
-                    Toast.makeText(MainActivity.this,"Incorrect Password or Username",Toast.LENGTH_SHORT).show();
-                }
+                ServerConnection server = new ServerConnection();
+                String result = server.checkConnection("1", "2", getApplicationContext());
+                Log.d(TAG, result);
+                Log.d(TAG, "Returned");
+//                // checking if the entered text is empty or not.
+//                if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(password)) {
+//                    Toast.makeText(MainActivity.this, "Please enter user name and password", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                if(userName.equals("admin") && password.equals("admin")){
+//                    //correct
+//                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+//                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+//                    i.putExtra("username", userName);
+//                    startActivity(i);
+//                }else{
+//                    Toast.makeText(MainActivity.this,"Incorrect Password or Username",Toast.LENGTH_SHORT).show();
+//                }
             }
         });
     }
--- app/src/main/java/com/example/cpen391/ServerConnection.java
+++ app/src/main/java/com/example/cpen391/ServerConnection.java
@@ -1,22 +1,34 @@
 package com.example.cpen391;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class ServerConnection {
 
     public final String VM_public_ip = "http://15.222.207.245:8000/";
     private final String TAG = "ServerConnection";
 
-    public int checkConnection(String SHA256_username, String SHA256_password){
+    public String checkConnection(String SHA256_username, String SHA256_password, Context context){
 
-        return -1;
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("username", SHA256_username);
+        params.put("password", SHA256_password);
+
+        return "here";
     }
 
     public int login(String SHA256_username, String SHA256_password){
@@ -39,8 +51,22 @@ public class ServerConnection {
         return -1;
     }
 
-    private int sendRequestToServer(JSONObject request){
-
-        return -1;
+    private void sendRequestToServer(HashMap<String, String> data, Context context){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.start();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, VM_public_ip + "login", new JSONObject(data),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "Unable to reload credit");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onErrorResponse updateUI " + "Error: " + error.getMessage());
+                    }
+                });
+        queue.add(request);
     }
 }
